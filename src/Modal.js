ffd33e75c7d7559e4ba282ef6dab7d44b71ff3e8
VnkHO <ho.vincent.nk@gmail.com>
2020-02-14 14:12:25 +0100

    reusable Modal

--- src/Modal.js
+++ src/Modal.js
@@ -12,8 +12,11 @@ export default class Modal extends Component {
                 {
                     on && (
                         <ModalWrapper>
-                            <button onClick={toggle}>Close</button>
-                            <div>{children}</div>
+                            <ModalCard>
+                                <CloseButton onClick={toggle}>Close</CloseButton>
+                                <div>{children}</div>
+                            </ModalCard>
+                            <BackgroundCard onClick={toggle} />
                         </ModalWrapper>
                     )}
             </Portal>
@@ -28,5 +31,34 @@ const ModalWrapper = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background: teal;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const ModalCard = styled.div`
+    position: relative;
+    padding: 20px;
+    min-width: 20rem;
+    background: white;
+    border-radius: 5px;
+    z-index: 10;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, .3);
+    margin-bottom: 10px;
+`
+
+const CloseButton = styled.button`
+    position: absolute;
+    top: 0;
+    right: 0;
+`;
+
+const BackgroundCard = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: black;
+    opacity: .3;
 `
